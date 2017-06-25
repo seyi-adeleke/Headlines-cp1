0b5b148a411385cba9a83bac051638164efb2a6a
seyi-adeleke <adetokunbo.adeleke@andela.com>
2017-06-25 08:17:43 +0100

    bug(footer):center-footer
    The footer should be centered correctly on all pages
    
    Finishes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="footer">
-    <div className="">
-      <p className="footer-text">Made With <i className="fa fa-heart" /> and <i className="fa fa-stack-overflow" /> by <a rel="noopener noreferrer" target="_blank" href="https://seyiadeleke.herokuapp.com">Seyi</a></p>
+  <footer>
+    <div className="footer" style={{ left: 1, right: 1 }}>
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+          <p className="text-center">Made With <i className="fa fa-heart" /> and <i className="fa fa-stack-overflow" /> by <a rel="noopener noreferrer" target="_blank" href="https://seyiadeleke.herokuapp.com">Seyi</a></p>
+        </div>
+      </div>
     </div>
   </footer>
 );
