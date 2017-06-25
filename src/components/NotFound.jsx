c846a014849789f2f8784d643b3d4e99d92cee15
seyi <seyiadeleke42@yahoo.com>
2017-06-25 11:19:45 +0100

    Feedback/ui issues (#22)
    * bug(footer):center-footer
    
    The footer should be centered correctly on all pages
    
    Finishes.
    
    * bug(social):
    
    Social icons should be spaced correctly
    
    Finishes
    
    * styles(hover):style-issues
    
    Hover colors looked off
    
    Finishes
    
    * gitignore irrelevant files
    
    * styles(404 page)Refactored 404 page
    
    Finishes.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 const NotFound = () => (
-  <h1>404.. This page is not found!</h1>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-6 col-md-offset-3 text-muted text-center">
+        <h1>Well done. You Broke it.</h1>
+        <h2>This page doesn't exist or some other horrible error has occured.</h2>
+        <h2>Here's Shaq and a cat. <a href="/">Go Back</a></h2>
+        <iframe src="https://giphy.com/embed/nNxT5qXR02FOM" width="480" height="399" allowFullScreen title="404" />
+      </div>
+    </div>
+  </div>
 );
 export default NotFound;
+
